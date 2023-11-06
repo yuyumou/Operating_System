6ce235c1041f89f65f8dca11ce61c2923bffa513
X-yang03 <1979954151@qq.com>
2023-11-06 21:55:11 +0800

    exercise2 do_fork

--- riscv64-ucore-labcodes-Xyang-2113301/lab4/kern/process/proc.c
+++ riscv64-ucore-labcodes-Xyang-2113301/lab4/kern/process/proc.c
@@ -106,6 +106,8 @@ alloc_proc(void) {
     proc->state = PROC_UNINIT;  
     // pid为未初始化值
     proc->pid = -1;
+    // 运行时间为0
+    proc->runs = 0;
     // 内核栈起始地址先设为0
     proc->kstack = NULL;
     // 需要调度
@@ -297,7 +299,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
+    //LAB4:EXERCISE2 2113301
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
@@ -316,12 +318,34 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
      */
 
     //    1. call alloc_proc to allocate a proc_struct
+    if((proc = alloc_proc()) == NULL){
+        goto fork_out;
+    }
     //    2. call setup_kstack to allocate a kernel stack for child process
+    if(setup_kstack(proc)){
+        goto bad_fork_cleanup_kstack;
+    }
     //    3. call copy_mm to dup OR share mm according clone_flag
+    
+    if(copy_mm(clone_flags,proc)){
+        goto bad_fork_cleanup_proc;
+    }
     //    4. call copy_thread to setup tf & context in proc_struct
+    copy_thread(proc,stack,tf);
     //    5. insert proc_struct into hash_list && proc_list
+    bool intr;
+    local_intr_save(intr);
+    list_add(list_prev(&proc_list),&(proc->list_link));
+    proc->parent = current;
+    proc->pid = get_pid();
+    hash_proc(proc);
+    nr_process++;
+    local_intr_restore(intr);
+
     //    6. call wakeup_proc to make the new child process RUNNABLE
+    wakeup_proc(proc);
     //    7. set ret vaule using child proc's pid
+    ret = proc->pid;
 
     
 
